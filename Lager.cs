80cb5eafdcd66959cdf9cbfb90bb462d8bec42a8
haii-trn <HaiLinh1920@icloud.com>
2025-02-26 10:06:08 +0100

    Klasse lager und Objekte erstellt

---
+++ Lager.cs
@@ -0,0 +1,9 @@
+public class Lager
+{ 
+  public int RaumNr;
+  public bool CharakterAnw;
+  public bool Access ;
+  public string[ ]Gegenstaende=new string[13];
+  public int[ ]Zugaenge=new int[4];
+
+}
