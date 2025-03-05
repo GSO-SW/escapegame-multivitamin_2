adda9321b608ee15b8e9a82c46eb7780d4edbc9d
haii-trn <142978318+haii-trn@users.noreply.github.com>
2025-03-05 08:35:13 +0100

    Update Program.cs
    main wiederhergestellt

--- Program.cs
+++ Program.cs
@@ -36,61 +36,6 @@ Freezer.Access =true;
 Freezer.Gegenstaende[0]="Freezer";
 Freezer.Zugaenge[0] =4;
 
-
- 
-     
-    {
-        Console.WriteLine("Hinweis: 2 Codes für den Freezer sind in diesem Lager versteckt. Suche nach Hinweisen!");
-        
-        RaetselSchalter();
-        RaetselSchrank();
-    }
-
-    void RaetselSchalter()
-    {
-        Console.WriteLine("Es gibt mehrere Schalter in diesem Raum. Finde die richtige Reihenfolge!");
-        int[] richtigeReihenfolge = { 2, 4, 1, 3 };
-        int[] eingabe = new int[4];
-        
-        for (int i = 0; i < 4; i++)
-        {
-            Console.Write("Schalter " + (i + 1) + ": ");
-            eingabe[i] = int.Parse(Console.ReadLine());
-        }
-        
-        if (Enumerable.SequenceEqual(eingabe, richtigeReihenfolge))
-        {
-            Console.WriteLine("Richtig! Du erhältst eine Nummer: 7");
-        }
-        else
-        {
-            Console.WriteLine("Falsche Reihenfolge! Versuche es erneut.");
-            RaetselSchalter();
-        }
-    }
-
-     void RaetselSchrank()
-    {
-        Console.WriteLine("Der Schrank ist geschlossen. Löse das Buchstaben-Zahlen-Code-Rätsel!");
-        Console.Write("");
-        Console.WriteLine("Hinweis: Welche Nummern haben die Buchstaben im Alphabet? (T = , L = , R = )");
-        Console.WriteLine("");
-        
-        Console.Write("Gib die richtige Zahlenkombination ein: ");
-        string eingabe = Console.ReadLine();
-        
-        if (eingabe == "201218")
-        {
-            Console.WriteLine("Richtig! Du erhältst eine Nummer: 3");
-        }
-        else
-        {
-            Console.WriteLine("Falsch! Versuche es erneut.");
-            RaetselSchrank();
-        }
-    }
-
-
 // Initialisierung
 
 
