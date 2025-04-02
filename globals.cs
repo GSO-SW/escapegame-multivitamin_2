9d6a504830390ff125721801d790f78f3e5e66c9
Donnel17 <ezomondonnel@gmail.com>
2025-04-02 10:52:48 +0200

    noch nicht fertig

---
+++ globals.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+    public static class Globals
+    {
+        public static int punkte = 0;
+        public static Random random = new Random();
+        public static Lager aktuellerRaum;
+
+
+    }
